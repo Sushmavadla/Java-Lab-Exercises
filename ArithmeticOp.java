ff0e482a6c4b00cf29c904f5197a09d5d0843f3a
Sushmavadla <sushmavadla27@gmail.com>
2024-05-30 16:19:35 +0530

    Add files via upload

---
+++ ArithmeticOp.java
@@ -0,0 +1,22 @@
+package operators;
+
+public class ArithmeticOp {
+	public static void main(String[] args) {
+		
+		 int sum = 10 + 5;
+	     int difference = 10 - 5;
+	     int product = 10 * 5;
+	     int quotient1 = 10 / 3; 
+	     double quotient2 = 10.0 / 3;
+	     int remainder = 10 % 3;
+	        System.out.println("Sum: " + sum);
+	        System.out.println("Difference: " + difference);
+	        System.out.println("Product: " + product);
+	        System.out.println("Quotient1: " + quotient1);
+	        System.out.println("Quotient2: " + quotient2);
+	        System.out.println("Remainder: " + remainder);
+
+	}
+
+}
+
