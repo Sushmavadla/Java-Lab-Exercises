a7ba8e04a9cd9fe70c8293f11257a890181f8ddc
Sushmavadla <sushmavadla27@gmail.com>
2024-05-30 16:43:32 +0530

    Add files via upload

---
+++ ArithAssignOpEx.java
@@ -0,0 +1,21 @@
+package operators;
+
+public class ArithAssignOpEx {
+	public static void main(String[] args) {
+		
+		int a=10,b=3;
+		a+=b;
+		System.out.println(a);
+		a-=b;
+		System.out.println(a);
+		a*=b;
+		System.out.println(a);
+		a=a/b;
+		System.out.println(a);
+		a=a%b;
+		System.out.println(a);
+		
+
+	}
+
+}
