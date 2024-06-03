0606cd847724e959ba82aee1226904474819d58f
Sushmavadla <sushmavadla27@gmail.com>
2024-06-03 19:44:58 +0530

    Add files via upload

---
+++ factorialrecur.java
@@ -0,0 +1,18 @@
+package junefirstweek;
+import java.util.Scanner;
+public class factorialrecur {
+	public static int calFact(int n) {
+		if(n==0||n==1) {
+			return 1;
+		}
+		return n*calFact(n-1);
+	}
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n=sc.nextInt();
+		int fact=calFact(n);
+		System.out.println("Factorial of "+n+" is: "+fact);
+		
+	}
+}
