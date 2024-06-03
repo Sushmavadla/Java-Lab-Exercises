67e83da1a017f6b2d4e2719962dadb70224b2839
Sushmavadla <sushmavadla27@gmail.com>
2024-06-03 16:35:19 +0530

    Add files via upload

---
+++ DoWhileEx.java
@@ -0,0 +1,13 @@
+package constructs;
+
+public class DoWhileEx {
+	public static void main(String[] args) {
+
+	     int a=4;
+	     do
+	     {
+	    	 a++;
+	    	 System.out.println(a);
+	     }while(a<10);
+		}
+}
---
+++ ForEx.java
@@ -0,0 +1,12 @@
+package constructs;
+
+public class ForEx {
+	public static void main(String[] args) 
+	{
+
+   for(int i=1;i<=7;i++)
+   {
+	   System.out.println(i);
+   }
+}
+}
