befda2e6de0b046be83a78f8757602bbc224fc1e
Sushmavadla <sushmavadla27@gmail.com>
2024-06-03 15:33:58 +0530

    Add files via upload

---
+++ LogicalNotEx.java
@@ -0,0 +1,18 @@
+package operators;
+
+public class LogicalNotEx {
+	public static void main(String[] args) {
+		  boolean condition = false;
+	        
+	        // Using logical NOT operator to reverse the condition
+	        if (!condition) 
+	        {
+	            System.out.println("Condition is false");
+	        } 
+	        else 
+	        {
+	            System.out.println("Condition is true");
+	        }
+
+	}
+}
