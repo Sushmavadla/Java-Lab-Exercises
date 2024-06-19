d276451c724b42d79c09497e2bb4df45415307bd
Sushmavadla <sushmavadla27@gmail.com>
2024-06-19 16:16:24 +0530

    Add files via upload

---
+++ ExceptionEx.java
@@ -0,0 +1,21 @@
+package exceptions;
+import java.util.*;
+public class ExceptionEx {
+	public static void main(String[] args) {
+		try
+		{
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter your age:");
+     int age=sc.nextInt();
+     
+	}
+		catch(InputMismatchException e)
+		{
+			System.out.println("Wrong input");
+		}
+		finally
+		{
+			System.out.println("Your are eligible");
+		}
+	}
+}
